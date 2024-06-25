c161a25752796b3f82289cc773454e2cafca285f
Jane Ro <janero95@gmail.com>
2024-06-25 19:39:21 +0900

    feat: club card item

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,15 @@
-import { Box, Container, Grid, Paper, Typography } from '@mui/material';
-
-const ADVICE_API_URL = 'https://api.adviceslip.com/advice';
+import { ArrowForward, Favorite } from '@mui/icons-material';
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  IconButton,
+  Typography,
+} from '@mui/material';
 
 function Home() {
   return (
@@ -11,16 +20,27 @@ function Home() {
         </Typography>
         <Grid container spacing={2} columns={{ xs: 1, sm: 8, md: 12 }}>
           <Grid item xs={1} sm={4} md={4}>
-            <Paper sx={{ p: 1, textAlign: 'center' }}>Club Item</Paper>
-          </Grid>
-          <Grid item xs={1} sm={4} md={4}>
-            <Paper sx={{ p: 1, textAlign: 'center' }}>Club Item</Paper>
-          </Grid>
-          <Grid item xs={1} sm={4} md={4}>
-            <Paper sx={{ p: 1, textAlign: 'center' }}>Club Item</Paper>
-          </Grid>
-          <Grid item xs={1} sm={4} md={4}>
-            <Paper sx={{ p: 1, textAlign: 'center' }}>Club Item</Paper>
+            <Card sx={{ height: 360, position: 'relative' }}>
+              <CardMedia
+                sx={{ height: 180 }}
+                image='images/club_1.jpg'
+                alt='club image'
+              />
+              <CardContent>
+                <Typography variant='h6'>Club Name</Typography>
+                <Typography variant='body2'>
+                  Creativiy | Community & Service
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <IconButton>
+                  <Favorite />
+                </IconButton>
+                <IconButton>
+                  <ArrowForward />
+                </IconButton>
+              </CardActions>
+            </Card>
           </Grid>
         </Grid>
       </Box>
