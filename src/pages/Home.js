edcc234538495c279c332b2da1297b300aef99de
Jane Ro <janero95@gmail.com>
2024-06-25 16:11:08 +0900

    chore: club list setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,26 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Grid, Paper, Typography } from '@mui/material';
 
 function Home() {
   return (
     <Container maxWidth='md' sx={{ height: '100vh' }}>
       <Box sx={{ my: 4 }}>
-        <Typography variant='h5'>Home</Typography>
+        <Typography variant='h5' mb={4}>
+          Home
+        </Typography>
+        <Grid container spacing={2} columns={{ xs: 1, sm: 8, md: 12 }}>
+          <Grid item xs={1} sm={4} md={4}>
+            <Paper sx={{ p: 1, textAlign: 'center' }}>Club Item</Paper>
+          </Grid>
+          <Grid item xs={1} sm={4} md={4}>
+            <Paper sx={{ p: 1, textAlign: 'center' }}>Club Item</Paper>
+          </Grid>
+          <Grid item xs={1} sm={4} md={4}>
+            <Paper sx={{ p: 1, textAlign: 'center' }}>Club Item</Paper>
+          </Grid>
+          <Grid item xs={1} sm={4} md={4}>
+            <Paper sx={{ p: 1, textAlign: 'center' }}>Club Item</Paper>
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
