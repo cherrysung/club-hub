93eecf64012e493c373555b7ddb163b4333de050
Jane Ro <janero95@gmail.com>
2024-07-02 17:06:35 +0900

    chore: add category constants

---
+++ src/lib/constants.js
@@ -0,0 +1,18 @@
+export const CAS_CATEGORIES = [
+  { label: 'Creativity', value: 'Creativity' },
+  { label: 'Creativity & Activity', value: 'Creativity & Activity' },
+  { label: 'Activity', value: 'Activity' },
+  { label: 'Activity & Service', value: 'Activity & Service' },
+  { label: 'Service', value: 'Service' },
+  { label: 'Creativity & Service', value: 'Creativity & Service' },
+  { label: 'All', value: 'All' },
+];
+
+export const ACTIVITY_CATEGORIES = [
+  { label: 'Debate', value: 'Debate' },
+  { label: 'Community Service', value: 'Community Service' },
+  { label: 'Sports', value: 'Sports' },
+  { label: 'Research', value: 'Research' },
+  { label: 'Media/Content Creation/Art', value: 'Media/Content Creation/Art' },
+  { label: 'Culture', value: 'Culture' },
+];
