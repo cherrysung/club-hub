c6bb559822a575bc825be443978c1029137145f7
cherrysung <jsung2025@chadwickschool.org>
2024-08-25 17:03:48 +0900

    subjects

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
-export const NO_IMAGE_SRC = '/images/no-image.png';
+export const NO_IMAGE_SRC = "/images/no-image.png";
 
 export const GRADE_ITEMS = [
   { value: 9, label: 9 },
@@ -8,71 +8,80 @@ export const GRADE_ITEMS = [
 ];
 
 export const CAS_CATEGORIES = [
-  { label: 'Creativity', value: 'Creativity' },
-  { label: 'Creativity & Activity', value: 'Creativity & Activity' },
-  { label: 'Activity', value: 'Activity' },
-  { label: 'Activity & Service', value: 'Activity & Service' },
-  { label: 'Service', value: 'Service' },
-  { label: 'Creativity & Service', value: 'Creativity & Service' },
-  { label: 'All', value: 'All' },
+  { label: "Creativity", value: "Creativity" },
+  { label: "Creativity & Activity", value: "Creativity & Activity" },
+  { label: "Activity", value: "Activity" },
+  { label: "Activity & Service", value: "Activity & Service" },
+  { label: "Service", value: "Service" },
+  { label: "Creativity & Service", value: "Creativity & Service" },
+  { label: "All", value: "All" },
 ];
 
 export const ACTIVITY_CATEGORIES = [
-  { label: 'Debate', value: 'Debate' },
-  { label: 'Community Service', value: 'Community Service' },
-  { label: 'Sports', value: 'Sports' },
-  { label: 'Research', value: 'Research' },
-  { label: 'Media/Content Creation/Art', value: 'Media/Content Creation/Art' },
-  { label: 'Culture', value: 'Culture' },
+  { label: "Debate", value: "Debate" },
+  { label: "Community Service", value: "Community Service" },
+  { label: "Sports", value: "Sports" },
+  { label: "Research", value: "Research" },
+  { label: "Media/Content Creation/Art", value: "Media/Content Creation/Art" },
+  { label: "Culture", value: "Culture" },
+];
+
+export const ROUNDSQUARE_IDEALS = [
+  { label: "Internationalism", value: "Internationalism" },
+  { label: "Democracy", value: "Democracy" },
+  { label: "Environmentalism", value: "Environmentalism" },
+  { label: "Adventure", value: "Adventure" },
+  { label: "Leadership", value: "Leadership" },
+  { label: "Service", value: "Service" },
 ];
 
 export const TARGET_SERVICE_CATEGORIES = [
-  { label: 'People with Disabilities', value: 'People with Disabilities' },
-  { label: 'Environment/Animals', value: 'Environment/Animals' },
-  { label: 'Elderly', value: 'Elderly' },
-  { label: 'Children', value: 'Children' },
-  { label: 'The Poor', value: 'The Poor' },
-  { label: 'Gender-Specific', value: 'Gender-Specific' },
-  { label: 'Race-Specific', value: 'Race-Specific' },
-  { label: 'Cultural', value: 'Cultural' },
-  { label: 'Mixed', value: 'Mixed' },
+  { label: "People with Disabilities", value: "People with Disabilities" },
+  { label: "Environment/Animals", value: "Environment/Animals" },
+  { label: "Elderly", value: "Elderly" },
+  { label: "Children", value: "Children" },
+  { label: "The Poor", value: "The Poor" },
+  { label: "Gender-Specific", value: "Gender-Specific" },
+  { label: "Race-Specific", value: "Race-Specific" },
+  { label: "Cultural", value: "Cultural" },
+  { label: "Mixed", value: "Mixed" },
 ];
 
 export const RELEVANT_SUBJECT_CATEGORIES = [
-  { label: 'English', value: 'English' },
-  { label: 'Mandarin', value: 'Mandarin' },
-  { label: 'Korean', value: 'Korean' },
-  { label: 'Spanish', value: 'Spanish' },
-  { label: 'I&S', value: 'I&S' },
-  { label: 'Business Management', value: 'Business Management' },
-  { label: 'Economics', value: 'Economics' },
-  { label: 'Global Politics', value: 'Global Politics' },
-  { label: 'Psychology', value: 'Psychology' },
-  { label: 'History', value: 'History' },
-  { label: 'Culture', value: 'Culture' },
-  { label: 'Biology', value: 'Biology' },
-  { label: 'Chemistry', value: 'Chemistry' },
-  { label: 'Physics', value: 'Physics' },
-  { label: 'Environmental Science', value: 'Environmental Science' },
-  { label: 'Math', value: 'Math' },
-  { label: 'Music', value: 'Music' },
-  { label: 'Theatre Arts', value: 'Theatre Arts' },
-  { label: 'Visual Arts', value: 'Visual Arts' },
-  { label: 'Film', value: 'Film' },
-  { label: 'Design', value: 'Design' },
-  { label: 'PHE', value: 'PHE' },
-  { label: 'Mixed', value: 'Mixed' },
+  { label: "English", value: "English" },
+  { label: "Mandarin", value: "Mandarin" },
+  { label: "Korean", value: "Korean" },
+  { label: "Spanish", value: "Spanish" },
+  { label: "Business Management", value: "Business Management" },
+  { label: "Economics", value: "Economics" },
+  { label: "Global Politics", value: "Global Politics" },
+  { label: "Psychology", value: "Psychology" },
+  { label: "History", value: "History" },
+  { label: "Culture", value: "Culture" },
+  { label: "Biology", value: "Biology" },
+  { label: "Chemistry", value: "Chemistry" },
+  { label: "Physics", value: "Physics" },
+  { label: "Environmental Science", value: "Environmental Science" },
+  { label: "Computer Science", value: "Computer Science" },
+  { label: "Math", value: "Math" },
+  { label: "Music", value: "Music" },
+  { label: "Theatre Arts", value: "Theatre Arts" },
+  { label: "Visual Arts", value: "Visual Arts" },
+  { label: "Film", value: "Film" },
+  { label: "Design", value: "Design" },
+  { label: "PHE", value: "PHE" },
+  { label: "Mixed (check description)", value: "Mixed (check description)" },
 ];
 
 export const TIME_COMM_OPTIONS = [
-  { label: 'Only club time', value: 0 },
-  { label: '+30 mins', value: 0.5 },
-  { label: '+1 hr', value: 1 },
-  { label: '+over 1 hr', value: 3 },
+  { label: "Only club time", value: 0 },
+  { label: "+30 mins", value: 0.5 },
+  { label: "+1 hr", value: 1 },
+  { label: "+over 1 hr", value: 3 },
 ];
 
 export const SEMESTER_CATEGORIES = [
-  { label: 'Semester 1', value: '1' },
-  { label: 'Semester 2', value: '2' },
-  { label: 'Both Semesters', value: 'both' },
+  { label: "Semester 1", value: "1" },
+  { label: "Semester 2", value: "2" },
+  { label: "Both Semesters", value: "both" },
 ];
